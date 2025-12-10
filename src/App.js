bf5ec7957776f421bdaff43c2e226fc5a204d7dc
Ananya <ananyars24@gmail.com>
2025-12-10 18:58:44 +0530

    Updated App.js content

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div style={{ textAlign: "center", marginTop: "50px" }}>
       <h1>Hello from My First React App 🚀</h1>
       <p>This app is connected to GitHub!</p>
+      <p>Checking for editing in vs code and move it to git!</p>
     </div>
   );
 }
